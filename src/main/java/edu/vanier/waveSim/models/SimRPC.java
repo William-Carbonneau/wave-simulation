a749e97c0658da482b8411bf190b4cbb6742edbc
Hildthelsta <131473217+Hildthelsta@users.noreply.github.com>
2023-10-30 15:21:14 -0400

    Blocked error message but still doesn't color

--- src/main/java/edu/vanier/waveSim/models/SimRPC.java
+++ src/main/java/edu/vanier/waveSim/models/SimRPC.java
@@ -26,11 +26,14 @@ public class SimRPC extends CellularLogic {
         super(operatingCanvas, widthX, heightY);
 
         //Initialize random color
+        /*
         for (int i = 0; i < widthX; i++) {
             for (int j = 0; j < heightY; j++) {
                 colorCell(i, j, colors[(int) (Math.random() * nreOfDifferentEntities)]);
             }
         }
+         */
+        InitializeRandomColor();
 
         if (scaling < 1 || scaling % 2 != 0) {
             logger.error("scaling is wrong, setting to 1 by default");
@@ -39,78 +42,83 @@ public class SimRPC extends CellularLogic {
         }
     }
 
-
-
     public void InitializeRandomColor() {
         for (int i = 0; i < scaledX; i++) {
             for (int j = 0; j < scaledY; j++) {
                 int color = (int) (Math.random() * nreOfDifferentEntities);
                 current[i][j] = color;
+                System.out.println(colors[color].getGreen());
                 colorCell(i, j, colors[color]);
             }
         }
     }
-    
+
     /*
     To Modify for my 
     verify scaled x and y
-    */
-        @Override
-    	public void simFrame() {
-		//int[][] newCells = cells;
-                this.nextFrame = this.current;
-		
-		for (int i = 0; i < scaledX; i++) {
-			for (int j = 0; j < scaledY; j++) {
-				
-				int predatorStates = (nreOfDifferentEntities - 0) / 2; //Standard: Make half (rounded down) of the colors predators to this color
-				int[] predators = new int[predatorStates];
-				int[] predatorState = new int[predatorStates];
-				int gesamtPredators = 0;
+     */
+    @Override
+    public void simFrame() {
+        //int[][] newCells = cells;
+        this.nextFrame = this.current;
 
-				//Count number if neighbour predators for each predator state
-				for (int k = 0; k < predatorStates; k++) {
-					predatorState[k] =(int)((current[i][j] + 1 + k) % nreOfDifferentEntities);
-					predators[k] = lookAround(i, j, colors[(int) predatorState[k]]);
-					gesamtPredators += predators[k];
-				}
-				
-				int randomMinimum = (int) (Math.random() * nreOfRandomPredator);
-				
-				//If there are more neighbour predators than the threshold, change current cell to a random predator cell (weighted)
-				if (gesamtPredators >= nreOfRandomPredator + randomMinimum) {
-					int r = (int) (Math.random() * gesamtPredators);
-					int k = -1;
-					while (r >= 0) {
-						k++;
-						r -= predators[k];
-					}
-					this.nextFrame[i][j] = predatorState[k];
-                                        colorCell(i, j, colors[predatorState[k]]);
-				}
-				
-			}
+        for (int i = 0; i < scaledX; i++) {
+            for (int j = 0; j < scaledY; j++) {
 
-		}
-		
-		this.current = this.nextFrame;
-	}
+                int predatorStates = (nreOfDifferentEntities - 0) / 2; //Standard: Make half (rounded down) of the colors predators to this color
+                int[] predators = new int[predatorStates];
+                int[] predatorState = new int[predatorStates];
+                int gesamtPredators = 0;
+
+                //Count number if neighbour predators for each predator state
+                for (int k = 0; k < predatorStates; k++) {
+                    predatorState[k] = (int) ((current[i][j] + 1 + k) % nreOfDifferentEntities);
+                    predators[k] = lookAround(i, j, colors[(int) predatorState[k]]);
+                    gesamtPredators += predators[k];
+                }
+
+                int randomMinimum = (int) (Math.random() * nreOfRandomPredator);
+
+                //If there are more neighbour predators than the threshold, change current cell to a random predator cell (weighted)
+                if (gesamtPredators >= nreOfRandomPredator + randomMinimum) {
+                    int r = (int) (Math.random() * gesamtPredators);
+                    int k = -1;
+                    while (r >= 0) {
+                        k++;
+                        r -= predators[k];
+                    }
+                    this.nextFrame[i][j] = predatorState[k];
+                    colorCell(i, j, colors[predatorState[k]]);
+                }
+
+            }
+
+        }
+
+        this.current = this.nextFrame;
+    }
 
     public int lookAround(int x, int y, Color color) {
         int c = 0;
         for (int i = 0; i < 3; i++) {
             for (int j = 0; j < 3; j++) {
                 if (i != 1 || j != 1) {
-                    if (getCellState(x + j - 1, y + i - 1) == nreOfDifferentEntities) {
+                    //Catches all the index that will be out of bound and ignore them
+                    try{
+                    if (colors[(int)(getCellState(x + j - 1, y + i - 1)*nreOfDifferentEntities)] == color) {
                         c++;
+                    }
+                    }
+                    catch(ArrayIndexOutOfBoundsException e){
                     }
                 }
             }
         }
-        return c;
+    
+    return c ;
     }
 
-    public float getCellState(int x, int y) {
-        return current[((y + scaledY) % scaledY)][(x + scaledX) % scaledX];
+public float getCellState(int x, int y) {
+        return current[x][y];
     }
 }
