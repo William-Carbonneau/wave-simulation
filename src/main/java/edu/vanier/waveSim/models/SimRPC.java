f79cc9dc7cef88dcd71fb2829209606621123133
Hildthelsta <131473217+Hildthelsta@users.noreply.github.com>
2023-11-09 09:35:44 -0500

    I'm losing hope

--- src/main/java/edu/vanier/waveSim/models/SimRPC.java
+++ src/main/java/edu/vanier/waveSim/models/SimRPC.java
@@ -4,6 +4,7 @@
  */
 package edu.vanier.waveSim.models;
 
+import java.util.Arrays;
 import java.util.Random;
 import java.util.logging.Level;
 import javafx.scene.canvas.Canvas;
@@ -18,17 +19,15 @@ import org.slf4j.LoggerFactory;
 public class SimRPC extends CellularLogic {
 
     private int frameNumber = 0;
-    private int nreOfDifferentEntities = 3;
+    private int nreOfDifferentEntities = 1;
     private int nreOfNeededPredator = 1;
     private int nreOfRandomPredator = 1;
     Color[] colors = {Color.ORANGE, Color.YELLOW, Color.RED, Color.BLUE, Color.PURPLE, Color.GREEN, Color.GRAY, Color.HOTPINK};
-    Color[] predatorsList = new Color[colors.length * 2];
     private final static Logger logger = LoggerFactory.getLogger(SimRPC.class);
     private Random random = new Random();
 
     public SimRPC(Canvas operatingCanvas, int widthX, int heightY, int scaling) {
         super(operatingCanvas, widthX, heightY);
-        // simFrame();
         if (scaling < 1 || scaling % 2 != 0) {
             logger.info("scaling is not set, setting to 1 by default");
         } else {
@@ -41,7 +40,7 @@ public class SimRPC extends CellularLogic {
             for (int j = 0; j < scaledY; j++) {
                 int color = (random.nextInt(nreOfDifferentEntities));
                 current[i][j] = (float) color;
-                colorCell(i, j, colors[color]);    
+                colorCell(i, j, colors[color]);
             }
         }
     }
@@ -56,52 +55,63 @@ public class SimRPC extends CellularLogic {
         if (hasInitialized == false) {
             InitializeRandomColor();
             hasInitialized = true;
-            for (int i = 0; i < 1; i++) {
-                for (int j = 0; i < nreOfDifferentEntities; i++) {
-                    predatorsList[j] = colors[j];
-                }
-            }
         }
 
         for (int i = 0; i < scaledX; i++) {
 
             for (int j = 0; j < scaledY; j++) {
-
-                int predatorStates = (nreOfDifferentEntities - 0) / 2;
-                //System.out.println("Predator n: " + predatorStates);
-                int[] predators = new int[predatorStates];
-                int nearPredators = 0;
-
-                //Count number if neighbour predators for each predator state
-                for (int k = 0; k < predatorStates; k++) {
-                    predators[k] = lookAround(i, j, predatorsList[k + 1]);
-                    nearPredators += predators[k];
-                }
-                System.out.println(nearPredators);
-                int randomMinimum = random.nextInt(nreOfRandomPredator);
-
-                //If there are more neighbour predators than the threshold, change current cell to a random predator cell (weighted)
-                if (nearPredators >= nreOfNeededPredator + randomMinimum && nearPredators > 0) {
-                    int r = random.nextInt(nearPredators);
-                    int k = -1;
-                    while (r >= 0) {
-                        k++;
-                        r -= predators[k];
-                    }
-                    if (k < nreOfDifferentEntities) {
-                        this.nextFrame[i][j] = (float) (k + 1);
-                    } else if (k >= nreOfDifferentEntities) {
-                        this.nextFrame[i][j] = (float) (k + 1 - nreOfDifferentEntities);
-                    }
-                    colorCell(i, j, predatorsList[k + 1]);
-                }
-
+                devouredOrNot(i, j);
             }
+        }
+
+        if (this.current.equals(this.nextFrame)) {
+            System.out.println("the same");
         }
         float[][] temp = this.current;
         this.current = this.nextFrame;
         this.nextFrame = temp;
         System.out.println("Frame " + frameNumber++);
+    }
+
+    /**
+     *
+     * @param x
+     * @param y
+     */
+    public void devouredOrNot(int x, int y) {
+
+        int predatorStates = (nreOfDifferentEntities - 0) / 2;
+        int[] predators = new int[predatorStates];
+        int[] predatorsInt = new int[predatorStates];
+        int nearPredators = 0;
+        int predatorColorInt;
+
+        //Count number if neighbour predators for each predator state
+        for (int k = 0; k < predatorStates; k++) {
+
+            predatorColorInt = (int) (getCellState(x, y) + 1 + k);
+            if (predatorColorInt >= nreOfDifferentEntities) {
+                predatorColorInt = predatorColorInt - nreOfDifferentEntities;
+            }
+            predatorsInt[k] = predatorColorInt;
+
+            predators[k] = lookAround(x, y, colors[predatorColorInt]);
+            nearPredators += predators[k];
+        }
+        int randomMinimum = random.nextInt(nreOfRandomPredator);
+
+        //If there are more neighbour predators than the threshold, change current cell to a random predator cell (weighted)
+        if (nearPredators >= nreOfNeededPredator + randomMinimum && nearPredators > 0) {
+            int r = random.nextInt(nearPredators);
+            int k = -1;
+            while (r >= 0) {
+                k++;
+                r -= predators[k];
+            }
+            this.nextFrame[x][y] = (float) predatorsInt[k];
+            colorCell(x, y, colors[predatorsInt[k]]);
+
+        }
 
     }
 
@@ -118,7 +128,6 @@ public class SimRPC extends CellularLogic {
                     } catch (ArrayIndexOutOfBoundsException e) {
                     }
                 }
-                System.out.println(c);
             }
         }
         return c;
