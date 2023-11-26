9e2e782a0dcd929577019d99f803dc7b2f62c5e6
thePhilosopherGOAT <loovdrish.sujore@gmail.com>
2023-11-25 19:33:20 -0500

    Amplitude inserted in wave sim tab

--- src/main/java/edu/vanier/waveSim/models/SimForestFire.java
+++ src/main/java/edu/vanier/waveSim/models/SimForestFire.java
@@ -1,7 +1,4 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package edu.vanier.waveSim.models;
 
 import java.util.ArrayList;
@@ -10,10 +7,6 @@ import javafx.scene.paint.Color;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-/**
- *
- * @author loovd
- */
 public class SimForestFire extends CellularLogic{
     private final static Logger logger = LoggerFactory.getLogger(SimForestFire.class);
     private boolean needToInitialize = true;
