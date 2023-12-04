afbeb75930401d7a9ebef692d66e7f893e833c93
Hildthelsta <131473217+Hildthelsta@users.noreply.github.com>
2023-12-04 12:08:08 -0500

    Documentation for Langton Ant

--- src/main/java/edu/vanier/waveSim/models/SimAnt.java
+++ src/main/java/edu/vanier/waveSim/models/SimAnt.java
@@ -11,16 +11,33 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 /**
+ * Langton's ant is a cellular automaton where an "ant" walks in the grid
+ * depending on the color/value of the cell, it turns right or left. White cells
+ * makes the ant turn right, while black cells makes the ant turn left.
  *
- * @author Unkno
+ * @author 2264570 - Dmitrii Cazacu
  */
 public class SimAnt extends CellularLogic {
 
     private final static Logger logger = LoggerFactory.getLogger(SimAnt.class);
+    /**
+     * Horizontal and Vertical position of the ant
+     */
     int xPosition, yPosition;
     Random random = new Random();
+    /**
+     * Direction that the ant faces. 0 = Up, 1 = Right, 2 = Down, 3 = Left
+     */
     int direction = 0;
 
+    /**
+     * Constructor to initialize the simulation
+     *
+     * @param operatingCanvas type Canvas (javaFX)
+     * @param widthX type int height of grid in pixels
+     * @param heightY type int height of grid in pixels
+     * @param scaling type int scaling of grid from GUI
+     */
     public SimAnt(Canvas operatingCanvas, int widthX, int heightY, int scaling) {
         super(operatingCanvas, widthX, heightY);
         if (scaling < 1 || scaling % 2 != 0) {
@@ -31,77 +48,86 @@ public class SimAnt extends CellularLogic {
 
     }
 
+    /*
+    * Implementation of simulation called at every frame
+     */
     @Override
     public void simFrame() {
         if (hasInitialized == false) {
             xPosition = random.nextInt(0, scaledX);
             yPosition = random.nextInt(0, scaledY);
             hasInitialized = true;
-            System.out.println(scaledX+ ", "+ scaledY);
+            System.out.println(scaledX + ", " + scaledY);
         }
         antMoves();
     }
 
+    /*
+     * Looks at the direction that the ant is facing and the cell on which it is on.
+     * Using this information, it will move in a certain direction. Then, color the cell
+     * on which it has previously been. Zero, a white cell, is right. One, a black cell, is left.
+     */
     public void antMoves() {
         if (direction == 0) {
-            direction = 1;
             if (current[xPosition][yPosition] == 0) {
+                direction = 1;
                 colorAntCell(xPosition, yPosition);
-                if (xPosition < scaledX-1) {
+                if (xPosition < scaledX - 1) {
                     xPosition++;
                     return;
                 }
             } else if (current[xPosition][yPosition] == 1) {
+                direction = 3;
                 colorAntCell(xPosition, yPosition);
                 if (xPosition > 0) {
                     xPosition--;
                     return;
                 }
             }
-        }
-        else if (direction == 1) {
-            direction = 2;
+        } else if (direction == 1) {
             if (current[xPosition][yPosition] == 0) {
+                direction = 2;
                 colorAntCell(xPosition, yPosition);
-                if (yPosition < scaledY-1) {
+                if (yPosition < scaledY - 1) {
                     yPosition++;
                     return;
                 }
             } else if (current[xPosition][yPosition] == 1) {
+                direction = 0;
                 colorAntCell(xPosition, yPosition);
                 if (yPosition > 0) {
                     yPosition--;
                     return;
                 }
             }
-        }
-        else if (direction == 2) {
-            direction = 3;
+        } else if (direction == 2) {
             if (current[xPosition][yPosition] == 0) {
+                direction = 3;
                 colorAntCell(xPosition, yPosition);
                 if (xPosition > 0) {
                     xPosition--;
                     return;
                 }
             } else if (current[xPosition][yPosition] == 1) {
+                direction = 1;
                 colorAntCell(xPosition, yPosition);
-                if (xPosition < scaledX-1) {
+                if (xPosition < scaledX - 1) {
                     xPosition++;
                     return;
                 }
             }
-        }
-        else if (direction == 3) {
-            direction = 0;
+        } else if (direction == 3) {
             if (current[xPosition][yPosition] == 0) {
+                direction = 0;
                 colorAntCell(xPosition, yPosition);
                 if (yPosition > 0) {
                     yPosition--;
                     return;
                 }
             } else if (current[xPosition][yPosition] == 1) {
+                direction = 2;
                 colorAntCell(xPosition, yPosition);
-                if (yPosition < scaledY-1) {
+                if (yPosition < scaledY - 1) {
                     yPosition++;
                     return;
                 }
@@ -109,6 +135,13 @@ public class SimAnt extends CellularLogic {
         }
     }
 
+    /**
+     * Color and change the value of the cell on which the ant was on depending
+     * on the current color of it.
+     *
+     * @param x The horizontal coordinate of the cell
+     * @param y The vertical coordinate of the cell
+     */
     public void colorAntCell(int x, int y) {
         if (current[x][y] == 0) {
             current[x][y] = 1;
@@ -119,8 +152,6 @@ public class SimAnt extends CellularLogic {
         }
     }
 
-
-    
     @Override
     public void setPoint(int x, int y) {
     }
